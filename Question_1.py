dd9e07f8eb729a52c9de5267be60cce459308a16
jakeperalta <Sushmina.Dhakal@triosstudent.com>
2023-04-18 10:23:21 -0400

    comments added

--- Question_1.py
+++ Question_1.py
@@ -4,20 +4,22 @@ print("2.Search the product.")
 print("3.Delete the product from the cart.")
 print("4.Quit.")
 
+#making an empty list
 shoppingCart={}
 
+#creating a loop to proceed with different options.
 while True:
     x=int(input("Enter your choice: "))
     if x==1:
         ee= int(input("Enter the number of items to be added in the cart: "))
         
-        
+        #limiting items to be entered
         for i in range(ee):
                 if len(shoppingCart)==5:
                  print("Cart is full.")
                  break
             
-            
+                 #entering products
                 product_name=input("Enter item:")
                 brand=input("Enter brand name:")
                 shoppingCart.update({product_name:brand})
@@ -27,6 +29,7 @@ while True:
     
     
     elif x==2:
+            #searching items
              f= input("Enter the item to be searched:")
              if f in shoppingCart:
                 print({product_name:brand})
@@ -34,6 +37,7 @@ while True:
                  print("No product exist with this name.")
             
     elif x==3:
+            #deleting items
             g= input("Enter item to delete:")
             del shoppingCart[g]
             print("item deleted")
@@ -41,6 +45,7 @@ while True:
                 print("Cart is empty, no item is found")
     
     elif x==4:
+        #exiting from system
         print("Exited.")
         break
         
